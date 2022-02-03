22622c92cc58359bb056c67d2dbfc3af7f5116dd
Somers B Matthews <somersbmatthews@gmail.com>
2022-02-02 19:42:58 -0600

    initial commit

---
+++ src/main/java/com/somersbmatthews/cxfjaxrsdemo/CxfJaxRsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.somersbmatthews.cxfjaxrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxfJaxRsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxfJaxRsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somersbmatthews/cxfjaxrsdemo/CxfJaxRsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somersbmatthews.cxfjaxrsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxfJaxRsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
